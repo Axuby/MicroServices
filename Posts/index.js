6f0860488c3fca4b0ce1f0fec2de03a30c15a9ec
Axuby <azubuinesamuel@gmail.com>
2021-10-30 22:27:55 +0100

    Created Client

--- Posts/index.js
+++ Posts/index.js
@@ -11,4 +11,11 @@ app.post('/posts', (req,res)=>{
         id,
         title
     }
-})
+
+      res.status(201).send(posts[id]);
+});
+
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
