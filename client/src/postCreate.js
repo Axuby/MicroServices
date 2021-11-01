dd145bf347a021cddacf456d2b92c7cddcd57b24
Axuby <azubuinesamuel@gmail.com>
2021-11-02 00:02:47 +0100

    Created postCreate

---
+++ client/src/postCreate.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function postCreate() {
+    return (
+        <div>
+            <form >
+            <div className='form-group'>
+                
+            </div>
+            </form>
+        </div>
+    )
+}
+
+export default postCreate
