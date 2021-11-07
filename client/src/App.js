e81d910e8808b71e132463ad0056baf1ccada581
Axuby <azubuinesamuel@gmail.com>
2021-11-07 23:58:53 +0100

    added Moderation and query services

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import PostCreate from './postCreate'
 function App() {
   return (
     <div className="App">
+
+      <h1> Title</h1>
       <PostCreate/>
       <PostComments/>
     </div>
--- client/src/postCreate.js
+++ client/src/postCreate.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 function postCreate() {
+
+    const onSubmit = ()=> {
+
+
+    }
+
+
     return (
         <div>
             <form >
             <div className='form-group'>
-                
+               <labe>Title</labe>
+               <input className='form-control'/>
             </div>
+
+            <button className='btn btn-primary' onSubmit={onSubmit}> Submit</button>
             </form>
         </div>
     )
