8971653265ca69253c0b3bd26b56da1a64ead582
Axuby <azubuinesamuel@gmail.com>
2021-11-07 23:47:59 +0100

    added Moderation and query services

---
+++ client/src/PostComments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostComments(props) {
+    return (
+        <div>
+           <l1>{props.comment}</l1> 
+        </div>
+    )
+}
+
+export default PostComments
