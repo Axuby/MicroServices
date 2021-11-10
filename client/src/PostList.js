315c1d73ed75c2e9d66563612d5636715ee2d1c1
Axuby <azubuinesamuel@gmail.com>
2021-11-11 00:00:12 +0100

    postList

---
+++ client/src/PostList.js
@@ -0,0 +1,16 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
+function PostList() {
+    const [post, setPosts] = useState({})
+    const fetch = ()=>{
+         axios.
+    }
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PostList
