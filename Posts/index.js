d8b7e1d12fe82dd7a044ba8e6f0611a5c9a6538b
Axuby <azubuinesamuel@gmail.com>
2022-03-21 03:55:10 +0100

    Kubernetes config deployments

--- Posts/index.js
+++ Posts/index.js
@@ -17,6 +17,7 @@ app.post('/posts', (req,res)=>{
 });
 
 
-app.listen(4000, () => {
-  console.log('Listening on 4000');
+app.listen(4200, () => {
+  console.log('v20')
+  console.log('Listening on 4200');
 });
